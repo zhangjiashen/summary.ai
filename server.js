5f5c69f98ab0e733e329b6900360dbf73b4bf981
Jiashen zhang <jiashenzzzzz@gmail.com>
2023-05-18 22:03:19 -0700

    Update server.js

--- server.js
+++ server.js
@@ -16,28 +16,28 @@ app.post('/summary', async (req, res) => {
   const { channel_id } = req.body;
   
  console.log(`channel id ${channel_id}`);
- console.log(`key ${process.env.YOUR_SLACK_ACCESS_TOKEN}`);
+//  console.log(`key ${process.env.YOUR_SLACK_ACCESS_TOKEN}`);
 
 
   try {
     // Use the Slack API to retrieve chat history
     const response = await axios.get('https://slack.com/api/conversations.history', {
       params: {
-        token: '${ process.env.YOUR_SLACK_ACCESS_TOKEN }',
-        channel: '${ channel_id }', // Replace with the appropriate channel ID
+        'token': '${ process.env.YOUR_SLACK_ACCESS_USER_TOKEN }',
+        'channel': '${ channel_id }', // Replace with the appropriate channel ID
       },
     });
 
     // Process the chat history and generate a summary
     // const summary = generateSummary(response.data.messages);
-    const msg = response.data.messages;
+    const msg = response.data;
     console.log(msg)
 
     // Use the Slack API to send the summary back to the user
     await axios.post('https://slack.com/api/chat.postMessage', {
-      token: '${ process.env.YOUR_SLACK_ACCESS_TOKEN }',
-      channel: '${ channel_id }',
-      text: 'I am Jiashen Zhang',
+      'token': '${ process.env.YOUR_SLACK_ACCESS_BOT_TOKEN }',
+      'channel': '${ channel_id }',
+      'text': 'I am Jiashen Zhang',
     });
 
     res.sendStatus(200);
