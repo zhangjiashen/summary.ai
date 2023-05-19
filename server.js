6245b4df046dc3d6429b35847f2273fbc8f9adb7
Jiashen zhang <jiashenzzzzz@gmail.com>
2023-05-18 21:03:32 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
