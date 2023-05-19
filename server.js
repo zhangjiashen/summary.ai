25caa98a7a72e93b6a379e8ff52f0114c5b6af3a
Jiashen zhang <jiashenzzzzz@gmail.com>
2023-05-18 19:08:02 -0700

    Update server.js

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.post('/summary', async (req, res) => {
 });
 
 // Start the server
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
